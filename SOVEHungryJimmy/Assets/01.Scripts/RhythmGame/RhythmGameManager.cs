67f05f68bbf4fce5f2fdc3dd9f670f584b8d8aea
handwashing <99708666+handwashing@users.noreply.github.com>
2022-07-07 20:24:22 +0900

    0707

--- SOVEHungryJimmy/Assets/01.Scripts/RhythmGame/RhythmGameManager.cs
+++ SOVEHungryJimmy/Assets/01.Scripts/RhythmGame/RhythmGameManager.cs
@@ -27,6 +27,16 @@ public class RhythmGameManager : MonoBehaviour
     public Text scoreText; //화면에 표시 될 점수 텍스트
     //public Text multiText; //화면에 표시 될 배속 속도 텍스트
 
+    public float totalNotes; //적중된 총 노트들?
+    public float normalHits;
+    public float goodHits;
+    public float perfectHits;
+    public float missedHits;
+
+    public GameObject resultsScreen; //점수 요소 가져다쓰려고 참조 쓰기 /점수 팝업으로 점수 보여주기
+    public Text percentHitText, normalsText, goodsText, perfectsText, missesText, rankText, finalScoreText; //점수판에 나타날 각 점수들
+
+
     void Start()
     {
         //음악 재생과 바 떨어뜨리기를 시작할 때 게임매니저를 컨트롤 
@@ -34,6 +44,10 @@ public class RhythmGameManager : MonoBehaviour
 
         scoreText.text = "Score: 0"; //처음 시작할 때 점수 = 0으로 
         //currentMultiplier = 1; //처음 시작할 때 획득 점수 = x 1
+
+         totalNotes = FindObjectsOfType<NoteObject>().Length;
+        //Arrow컴퍼넌트에 이미 NoteObject 스크립트가 있음 -> 여기서 Length 가져오기
+        //Length가 노트가 얼마나 찍혔는지 알려줄 것
     }
 
 
@@ -48,9 +62,53 @@ public class RhythmGameManager : MonoBehaviour
                 theBS.hasStarted = true; //BeatScroller또한 시작
 
                 //버튼을 눌렀을 때 음악이 플레잉 되길 원함
-                theMusic.Play();
+                theMusic .Play();
             }
         }
+         else
+            {
+                if(!theMusic.isPlaying && !resultsScreen.activeInHierarchy) //음악이 플레이 되지 않고, 점수판이 활성화 되지 않았다면...
+                {
+                    resultsScreen.SetActive(true); //점수판 활성화
+
+                    normalsText.text = "" + normalHits; //위 아래 같은 방식임?
+                    goodsText.text = goodHits.ToString();
+                    perfectsText.text = perfectHits.ToString();
+                    missesText.text = "" + missedHits;
+
+                    float totalHit = normalHits + goodHits + perfectHits; //총 점수 받아오기
+                    float percentHit = (totalHit / totalNotes) * 100f; //퍼센트로 (획득한 총 점수 / 만점?) 나타내기
+                
+                    percentHitText.text = percentHit.ToString("F1") + "%"; //한 자릿 수 소수점까지만 보여주기
+
+                    string rankVal = "F"; //default rank value
+
+                    if(percentHit > 40)
+                    {
+                        rankVal = "D";
+                        if(percentHit > 55)
+                        {
+                            rankVal = "C";
+                            if(percentHit > 70)
+                            {
+                                rankVal = "B";
+                                if(percentHit > 85)
+                                {
+                                    rankVal = "A";
+                                    if(percentHit > 95)
+                                    {
+                                        rankVal = "S";
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    rankText.text = rankVal; //점수판에 나타날  최종 랭크
+
+                    finalScoreText.text = currentScore.ToString(); //최종 점수
+                }
+            }
     }
 
     public void NoteHit()
@@ -77,22 +135,28 @@ public class RhythmGameManager : MonoBehaviour
         scoreText.text = "Score: " + currentScore; //(점수가 추가된) 현재 점수 표시
     }
 
-    public void NormalHit()
+   public void NormalHit()
     {
         currentScore += scorePerNote;
         NoteHit();
+
+        normalHits++; //총 스코어인 NormalHits에 1 합산
     }
 
     public void GoodHit()
     {
         currentScore += scorePerGoodNote;
         NoteHit();
+
+        goodHits++; //총 스코어인 GoodHits에 1 합산
     }
 
     public void PerfectHit()
     {
         currentScore += scorePerPerfectNote;
         NoteHit();
+
+        perfectHits++; //총 스코어인 PerfectHits에 1 합산
     }
 
     public void NoteMissed()
@@ -103,5 +167,7 @@ public class RhythmGameManager : MonoBehaviour
         // multiplierTracker = 0;
 
         // multiText.text = "Multiplier:x" + currentMultiplier;
+
+        missedHits++; //총 스코어인 missedHits에 1 합산
     }
 }
